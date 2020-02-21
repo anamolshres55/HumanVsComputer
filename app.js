f53de793c7ce609fd6f0eb8f1c49b719da27a28e
Anmol Shrestha <anamolshres55@gmail.com>
2020-02-21 13:11:43 +0545

    Refactored the code

--- app.js
+++ app.js
@@ -8,6 +8,86 @@ new Vue({
         gameIsRunning: false,
     },
 
+    methods:{
+
+        startGame(){
+            this.gameIsRunning = true,
+            this.humanHealth = 100,
+            this.computerHealth = 100
+        },
+
+        attack(){
+            
+            this.computerHealth -= this.calculateDamage(3,10);
+
+            if (this.checkWin()){
+
+                return;
+
+            }
+
+            this.humanHealth -= this.calculateDamage(5,12);
+
+            this.checkWin();
+
+        },
+
+        specialAttack(){
+
+            
+
+        },
+
+        heal(){
+
+
+        },
+
+        giveUp(){
+
+
+        },
+
+        calculateDamage(min, max){
+
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+
+        },
+
+        checkWin(){
+
+            if(this.computerHealth <= 0){
+                if(confirm('You won! Another Round?')){
+
+                    this.startGame();
+
+                }
+                else{
+
+                    this.gameIsRunning = false;
+
+                }
+                return true;
+            }
+
+            else if(this.humanHealth <= 0){
+                if(confirm('You lost, jokes on you! Another Round?')){
+
+                    this.startGame();
+
+                }
+                else{
+
+                    this.gameIsRunning = false;
+
+                }
+                return true;
+            }
+            
+            return false;
+        }
+    }
+
 
     
 });
